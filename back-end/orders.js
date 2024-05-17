607010bcf4a6e1dfd2b7d2fd5bd06945ec21dda9
phammaianh04 <sunny31122004@gmail.com>
2024-05-17 10:37:16 +0700

    17/5

---
+++ back-end/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Tố Ngọc Hoàn Plus +',
+        productNumber: '85',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Sữa rửa mặt Dova Care',
+        productNumber: '97',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Collagen',
+        productNumber: '3600',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
